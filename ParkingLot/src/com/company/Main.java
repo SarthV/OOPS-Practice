63551f28fab7b96022e3a7fd186f2ed048787e68
SarthV <22sarthvitekar@gmail.com>
2021-09-17 15:44:16 +0530

    Refined the parking lot

--- ParkingLot/src/com/company/Main.java
+++ ParkingLot/src/com/company/Main.java
@@ -1,11 +1,19 @@
 package com.company;
 
+import com.company.vehicle.Type;
+import com.company.vehicle.Vehicle;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        ParkingLot lot = new ParkingLot();
-        ParkingSpace[][] parkingLotCopy = lot.initializeLot();
-        lot.printLot();
+        Vehicle myVehicle = new Vehicle("Ciaz", Type.CAR);
+        Vehicle myVehicle2 = new Vehicle("Merc", Type.CAR);
+        RealDeal.startTheLot();
+        RealDeal.checkAndInsert(0,0,myVehicle);
+        RealDeal.checkAndInsert(2,2,myVehicle2);
+        RealDeal.removeVehicle(0,0,myVehicle);
+        // And so on.....
+
     }
 }
--- ParkingLot/src/com/company/ParkingLot.java
+++ ParkingLot/src/com/company/ParkingLot.java
@@ -2,57 +2,28 @@ package com.company;
 
 import com.company.vehicle.Vehicle;
 
-public class ParkingLot {
-    ParkingSpace[][] parkingLot;
-    final static int n = 5;
-    public ParkingSpace[][] initializeLot(){
-        parkingLot = new ParkingSpace[n][n];
+public class ParkingLot{
+
+    public static ParkingSpace[][] initializeLot(int n){
+        ParkingSpace[][] parkingLot = new ParkingSpace[n][n];
         for(ParkingSpace[] row : parkingLot){
             for(ParkingSpace col : row) {
                 col = new ParkingSpace();
+                //System.out.print(col.getVehicle().getModel());
             }
+            //System.out.println();
         }
         return parkingLot;
     }
-    public void printLot(){
+    public static void printLot(ParkingSpace[][] parkingLot){
         for(ParkingSpace[] row : parkingLot){
             for(ParkingSpace col : row) {
-                System.out.print("_ ");
+                if(col.isEmpty())System.out.print("_ ");
+                else{
+                    System.out.print(col.getVehicle().getModel().charAt(0));
+                }
             }
             System.out.println();
-        }
-    }
-
-    public boolean checkAndInsert(int row, int col, Vehicle vehicle) throws IllegalArgumentException{
-        if(row < 0 || row >= n || col < 0 || col >= 50){
-            throw new IllegalArgumentException("Invalid Cell");
-        }
-        ParkingSpace space = parkingLot[row][col];
-        if(!space.isEmpty()){
-            throw new IllegalArgumentException("Already occupied");
-        }
-        else{
-            space.setEmpty(false);
-            space.setVehicle(vehicle);
-            return true;
-        }
-    }
-
-    public boolean removeVehicle(int row, int col, Vehicle vehicle) throws IllegalArgumentException{
-        if(row < 0 || row >= n || col < 0 || col >= 50){
-            throw new IllegalArgumentException("Invalid Cell");
-        }
-        ParkingSpace space = parkingLot[row][col];
-        if(space.isEmpty()){
-            throw new IllegalArgumentException("Empty Cell!");
-        }
-        if(space.getVehicle() != vehicle){
-            throw new IllegalArgumentException("Not your vehicle");
-        }
-        else{
-            space.setEmpty(true);
-            space.setVehicle(new Vehicle());
-            return true;
         }
     }
 }
